1a90331a2246d17c6aa9bc3c5f2310e4f6376e3e
Heitor Torres <168280940+kansasgrind@users.noreply.github.com>
2026-03-26 10:17:49 -0300

    Atualização do Programa, feito na sala
    Cores customizadas, tela C, imagem personalizada na tela B e mais coisas legais ✌

--- atividades/MainActivity.kt
+++ atividades/MainActivity.kt
@@ -10,14 +10,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
@@ -32,7 +33,7 @@ class MainActivity : ComponentActivity() {
             ComposeNavigationAppTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = colorResource(id = R.color.olive)
                 ) {
                     AppNavigation()
                 }
@@ -48,9 +49,14 @@ fun AppNavigation() {
         composable("screen_a") {
             ScreenA(navController = navController)
         }
-        composable("screen_b?message={message}") {
+        composable("screen_b?message={message}&imageName={imageName}") {
             val message = it.arguments?.getString("message")
-            ScreenB(navController = navController, message = message)
+            val imageName = it.arguments?.getString("imageName")
+            ScreenB(navController = navController, message = message, imageName = imageName)
+        }
+        composable("screen_c?message={message}"){
+            val message = it.arguments?.getString("message")
+            ScreenC(navController=navController, message = message)
         }
     }
 }
@@ -72,42 +78,82 @@ fun ScreenA(navController: NavController) {
         )
 
         Text(text = "Bem-vindo a Blackjack Records!")
-        Button(onClick = { navController.navigate("screen_b?message=Por enquanto, nada disponível, sorry") }) {
+        Button(onClick = { navController.navigate("screen_b?message=Disco Akuma no Uta&imageName=disco") },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = colorResource(id = R.color.red)
+            )) {
             Text("Ver nossos produtos")
         }
-        Button(onClick = { navController.navigate("screen_b?message=Email: suporte@blackjack.com") }) {
+        Button(onClick = { navController.navigate("screen_b?message=Email: suporte@blackjack.com") },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = colorResource(id = R.color.red)
+            )) {
             Text("Informações para contato")
         }
     }
 }
 
 @Composable
-fun ScreenB(navController: NavController, message: String?) {
+fun ScreenB(navController: NavController, message: String?, imageName: String?) {
+
+    val context = androidx.compose.ui.platform.LocalContext.current
+
+    val imageResId = imageName?.let {
+        context.resources.getIdentifier(it, "drawable", context.packageName)
+    }
+
     Column(
         modifier = Modifier.fillMaxSize().padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-    ) {
+    )
+    {
+        if (imageResId != null && imageResId != 0) {
+            Image(
+                painter = painterResource(id = imageResId),
+                contentDescription = "imagem do produto",
+                modifier = Modifier
+                    .size(150.dp)
+                    .padding(bottom = 16.dp)
+            )
+        }
+
         message?.let { Text(text = " $it") }
-        Button(onClick = { navController.popBackStack() }) {
+
+        Button(onClick = { navController.navigate("screen_c?message=Serviço de Pagamento Indisponível") },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = colorResource(id = R.color.red)
+            )) {
+            Text("Comprar")
+        }
+
+        Button(onClick = { navController.popBackStack() },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = colorResource(id = R.color.red)
+            )) {
             Text("Voltar para home")
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun ScreenAPreview() {
-    ComposeNavigationAppTheme {
-        ScreenA(rememberNavController())
+fun ScreenC(navController: NavController, message: String?) {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        message?.let { Text(text = " $it") }
+
+        Button(onClick = { navController.popBackStack() },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = colorResource(id = R.color.red)
+            )) {
+            Text("Voltar")
+        }
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun ScreenBPreview() {
-    ComposeNavigationAppTheme {
-        ScreenB(rememberNavController(), "Olá da Tela A")
-    }
-}
+
 
